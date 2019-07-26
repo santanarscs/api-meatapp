945dc82cfc8e86248d1282d9de9046c26febe6d4
santanarscs <raphaelstn@gmail.com>
2019-07-26 14:25:33 -0300

    challenge 03 bootcamp

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,9 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import MeetupController from './app/controllers/MeetupController';
+import OrganizingController from './app/controllers/OrganizingController';
+import SubscriptionController from './app/controllers/SubscriptionController';
 import authMiddleware from './app/middleware/auth';
 
 const routes = new Router();
@@ -17,4 +20,14 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/meetups', MeetupController.index);
+routes.post('/meetups', MeetupController.store);
+routes.put('/meetups/:id', MeetupController.update);
+routes.delete('/meetups/:id', MeetupController.delete);
+
+routes.get('/organizing', OrganizingController.index);
+routes.get('/subscriptions', SubscriptionController.index);
+routes.post('/meetups/:meetupId/subscriptions', SubscriptionController.store);
+
 export default routes;
